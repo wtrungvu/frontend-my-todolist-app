03d1adaad4bad9ef8050251da950681e6f543ba0
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-29 18:08:10 +0700

    Add frontend project files 🎉

---
+++ src/Constants.js
@@ -0,0 +1,7 @@
+//Local
+//export const API_URL = 'http://localhost:8080'
+//export const JPA_API_URL = 'http://localhost:8080/jpa'
+
+//Prod
+export const API_URL = 'https://backend-my-todolist-app.herokuapp.com'
+export const JPA_API_URL = 'https://backend-my-todolist-app.herokuapp.com/jpa'
---
+++ src/components/todo/LoginComponent.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+import AuthenticationService from './AuthenticationService.js'
+
+class LoginComponent extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: '',
+            password: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false
+        }
+        // this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        // this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
+    }
+
+    handleChange(event) {
+        //console.log(this.state);
+        this.setState(
+            {
+                [event.target.name]
+                    : event.target.value
+            }
+        )
+    }
+
+    // handleUsernameChange(event) {
+    //     console.log(event.target.name);
+    //     this.setState(
+    //         {
+    //             [event.target.name]
+    //               :event.target.value
+    //         }
+    //     )
+    // }
+
+    // handlePasswordChange(event) {
+    //     console.log(event.target.value);
+    //     this.setState({password:event.target.value})
+    // }
+
+    loginClicked() {
+        //in28minutes,dummy
+        // if(this.state.username==='username' && this.state.password==='password'){
+        //     AuthenticationService.registerSuccessfulLogin(this.state.username,this.state.password)
+        //     this.props.history.push(`/welcome/${this.state.username}`)
+        //     //this.setState({showSuccessMessage:true})
+        //     //this.setState({hasLoginFailed:false})
+        // }
+        // else {
+        //     this.setState({showSuccessMessage:false})
+        //     this.setState({hasLoginFailed:true})
+        // }
+
+        // AuthenticationService
+        // .executeBasicAuthenticationService(this.state.username, this.state.password)
+        // .then(() => {
+        //     AuthenticationService.registerSuccessfulLogin(this.state.username,this.state.password)
+        //     this.props.history.push(`/welcome/${this.state.username}`)
+        // }).catch( () =>{
+        //     this.setState({showSuccessMessage:false})
+        //     this.setState({hasLoginFailed:true})
+        // })
+        AuthenticationService
+            .executeJwtAuthenticationService(this.state.username, this.state.password)
+            .then((response) => {
+                AuthenticationService.registerSuccessfulLoginForJwt(this.state.username, response.data.token)
+                this.props.history.push(`/todos`)
+            }).catch(() => {
+                this.setState({ showSuccessMessage: false })
+                this.setState({ hasLoginFailed: true })
+            })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+                <div className="container">
+                    {/*<ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>*/}
+                    {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
+                    {this.state.showSuccessMessage && <div>Login Sucessful</div>}
+                    {/*<ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>*/}
+                    <label for="uname"><b>Username:</b></label>
+                    <input type="text" placeholder="Enter 'username'" name="username" value={this.state.username} onChange={this.handleChange} />
+                    <br/>
+                    <label for="psw"><b>Password:</b></label>
+                    <input type="password" placeholder="Enter 'password'" name="password" value={this.state.password} onChange={this.handleChange} />
+                    <br/>
+                    <button className="btn btn-success" onClick={this.loginClicked}>Login</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LoginComponent
